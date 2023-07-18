a5cdbe55f9bcd2c847b3f76b4092d25a613fb5ce
Stone Skull <stoneskull@gmail.com>
2023-07-19 08:04:18 +1000

    Update site

--- _module.js
+++ _module.js
@@ -6479,13 +6479,17 @@ function create_fragment$b(ctx) {
 					"title": "stOneskull.net"
 				},
 				site_nav: [
-					{ "link": { "url": "/", "label": "About" } },
 					{
-						"link": { "url": "/", "label": "Product" }
+						"link": { "url": "/about", "label": "About" }
 					},
-					{ "link": { "url": "/", "label": "Why" } },
 					{
-						"link": { "url": "/", "label": "Pricing" }
+						"link": { "url": "/product", "label": "Product" }
+					},
+					{
+						"link": { "url": "/why", "label": "Why" }
+					},
+					{
+						"link": { "url": "/pricing", "label": "Pricing" }
 					},
 					{
 						"link": { "url": "/video", "label": "video" }
--- about/_module.js
+++ about/_module.js
@@ -6046,13 +6046,17 @@ function create_fragment$9(ctx) {
 					"title": "stOneskull.net"
 				},
 				site_nav: [
-					{ "link": { "url": "/", "label": "About" } },
 					{
-						"link": { "url": "/", "label": "Product" }
+						"link": { "url": "/about", "label": "About" }
 					},
-					{ "link": { "url": "/", "label": "Why" } },
 					{
-						"link": { "url": "/", "label": "Pricing" }
+						"link": { "url": "/product", "label": "Product" }
+					},
+					{
+						"link": { "url": "/why", "label": "Why" }
+					},
+					{
+						"link": { "url": "/pricing", "label": "Pricing" }
 					},
 					{
 						"link": { "url": "/video", "label": "video" }
